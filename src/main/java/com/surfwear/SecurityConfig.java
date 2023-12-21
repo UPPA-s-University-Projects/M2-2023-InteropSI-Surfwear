b5c0c111228544362c2d8cb7957e05e51686928f
Clément Combier <clement.combier@etud.univ-pau.fr>
2023-12-21 12:49:15 +0100

    New action to push to aws ECS

---
+++ src/main/java/com/surfwear/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.surfwear;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    /*@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic(); // Ou toute autre configuration d'authentification nécessaire
+
+        return http.build();
+    }*/
+}
